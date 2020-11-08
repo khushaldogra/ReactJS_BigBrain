7f8d092585b895d7f392dcff40e1fb794085f181
Angus Feng <z5207356@unsw.edu.au>
2020-11-08 20:00:56 +1100

    Can add empty questions to quiz

--- frontend/src/components/EditGame.jsx
+++ frontend/src/components/EditGame.jsx
@@ -1,25 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { Button } from 'semantic-ui-react'
+import { Button, Card } from 'semantic-ui-react'
 import { useParams } from 'react-router-dom';
 import config from '../config'
+import QuestionCard from './QuestionCard';
 
 function Edit() {
-    const { id } = useParams();
-    let quizName = '';
-    let questions = [];
+    const quizID = useParams().id;
+    let quizName = ''; // useState?
     let thumbnail = null;
+    const [questions, setQuestions] = useState([]);
+    const [questionChange, setQuestionChange] = useState(true);
 
     // Fetch questions in quiz
     // Maybe an unecessary fetch
     useEffect(() => {
         const token = localStorage.getItem('token');
+        // console.log(token);
         const options = {
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${token}`
             }
         }
-        const path = `${config.basePath}/admin/quiz/${id}`;
+        const path = `${config.basePath}/admin/quiz/${quizID}`;
         fetch(path, options)
             .then(res => {
                 if (!res.ok) {
@@ -30,25 +33,64 @@ function Edit() {
             .then(json => {
                 quizName = json.name;
                 thumbnail = json.thumbnail;
-                questions = json.questions;
+                setQuestions(json.questions);
                 console.log(json);
             })
             .catch(err => {
                 console.log(err);
             })
-    }, []);
+    }, [questionChange]);
 
+    // Create an empty question json template
+    const emptyQuestion = (id) => {
+        return {
+            'type': 'Empty Type',
+            'question': 'Empty Question',
+            'time': 0,
+            'points': 0,
+            'URL': null,
+            'answers': null,
+            'correctAnswer': null,
+            'questionID': id
+        }
+    }
+
+    // Adds an empty question to the quiz
     const addQuestion = () => {
+        const token = localStorage.getItem('token');
         const payload = {
-            'questions': {},
+            'questions': [...questions, emptyQuestion(questions.length + 1)],
             'name': quizName,
             'thumbnail': thumbnail
         }
+        const options = {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json' // FORGOT THIS
+            },
+            body: JSON.stringify(payload)
+        }
+        const path = `${config.basePath}/admin/quiz/${quizID}`;
+        fetch(path, options)
+            .then(res => {
+                if (!res.ok) {
+                    throw res;
+                }
+                console.log('Good put');
+                setQuestionChange(!questionChange);
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 
     return (
         <div>
             <Button onClick={addQuestion}>Add Question</Button>
+            <Card.Group>
+                {questions.map((q) => (<QuestionCard key={q.questionID} json={q}/>))}
+            </Card.Group>
         </div>
     )
 }
--- frontend/src/components/QuestionCard.jsx
+++ frontend/src/components/QuestionCard.jsx
@@ -1,16 +1,35 @@
-import React, { useState } from 'react';
-import { Card } from 'semantic-ui-react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Button } from 'semantic-ui-react'
 
-function QuestionCard() {
+function QuestionCard(props) {
+    const json = props.json;
+    console.log(props.json);
+
+    const deleteQuestion = () => {
+        // const token = localStorage.getItem('token');
+        // const payload = {
+        //     'questions': [... questions, emptyQuestion()],
+        //     'name': quizName,
+        //     'thumbnail': thumbnail
+        // }
+        console.log('delete');
+    }
+
     return (
         <Card>
             <Card.Content>
-                <Card.Header content='Jake Smith' />
+                <Card.Header content={json.question} />
                 <Card.Meta content='Musicians' />
                 <Card.Description content='Jake is a drummer living in New York.' />
+                <Button onClick={deleteQuestion}>Delete Question</Button>
             </Card.Content>
         </Card>
     )
+}
+
+QuestionCard.propTypes = {
+  json : PropTypes.any
 }
 
 export default QuestionCard;
