891d3df07b65d1b1d8c93cb29c866873f116e244
Angus Feng <z5207356@unsw.edu.au>
2020-11-09 16:38:09 +1100

    able to delete questions

--- frontend/src/components/EditGame.jsx
+++ frontend/src/components/EditGame.jsx
@@ -1,21 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Card } from 'semantic-ui-react'
 import { useParams } from 'react-router-dom';
 import config from '../config'
 import QuestionCard from './QuestionCard';
+import { Button, Card } from 'semantic-ui-react'
 
 function Edit() {
     const quizID = useParams().id;
     let quizName = ''; // useState?
     let thumbnail = null;
     const [questions, setQuestions] = useState([]);
+    const [questionIDs, setQuestionIDs] = useState(new Set()); // useState?
     const [questionChange, setQuestionChange] = useState(true);
 
     // Fetch questions in quiz
     // Maybe an unecessary fetch
     useEffect(() => {
         const token = localStorage.getItem('token');
-        // console.log(token);
         const options = {
             method: 'GET',
             headers: {
@@ -34,6 +34,13 @@ function Edit() {
                 quizName = json.name;
                 thumbnail = json.thumbnail;
                 setQuestions(json.questions);
+                // Update the question id set
+                let existingIDs = new Set();
+                for (const q of json.questions) {
+                    existingIDs.add(q.questionID);
+                }
+                setQuestionIDs(existingIDs);
+                console.log(questionIDs);
                 console.log(json);
             })
             .catch(err => {
@@ -55,11 +62,21 @@ function Edit() {
         }
     }
 
+    // Return a unique question id
+    const getQuestionID = () => {
+        let n = 0;
+        while (questionIDs.has(n)) {
+            n++;
+        }
+        return n;
+    }
+
     // Adds an empty question to the quiz
     const addQuestion = () => {
         const token = localStorage.getItem('token');
+        const uniqueID = getQuestionID();
         const payload = {
-            'questions': [...questions, emptyQuestion(questions.length + 1)],
+            'questions': [...questions, emptyQuestion(uniqueID)],
             'name': quizName,
             'thumbnail': thumbnail
         }
@@ -89,7 +106,14 @@ function Edit() {
         <div>
             <Button onClick={addQuestion}>Add Question</Button>
             <Card.Group>
-                {questions.map((q) => (<QuestionCard key={q.questionID} json={q}/>))}
+                {questions.map((q) => (<QuestionCard 
+                                        key={q.questionID} 
+                                        json={q}
+                                        questionChange={questionChange}
+                                        setQuestionChange={setQuestionChange}
+                                        questions={questions} // For fetch
+                                        quizName={quizName} 
+                                        thumbnail={thumbnail} />))}
             </Card.Group>
         </div>
     )
--- frontend/src/components/QuestionCard.jsx
+++ frontend/src/components/QuestionCard.jsx
@@ -1,27 +1,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Button } from 'semantic-ui-react'
+import { useParams } from 'react-router-dom';
+import { Card, Button } from 'semantic-ui-react';
+import config from '../config';
 
 function QuestionCard(props) {
+    const quizID = useParams().id;
     const json = props.json;
-    console.log(props.json);
+    // console.log(props.json);
+    // console.log(json.questionID);
 
+    // Return new list with question removed
+    const getNewQuestions = () => {
+        const newQuestions = [...props.questions];
+        let index = 0;
+        for (const question of newQuestions) {
+            if (question.questionID === json.questionID) {
+                index = newQuestions.indexOf(question);
+            }
+        }
+        newQuestions.splice(index, 1);
+        return newQuestions;
+    }
+
+    // Deletes a question from quiz
     const deleteQuestion = () => {
-        // const token = localStorage.getItem('token');
-        // const payload = {
-        //     'questions': [... questions, emptyQuestion()],
-        //     'name': quizName,
-        //     'thumbnail': thumbnail
-        // }
         console.log('delete');
+        const token = localStorage.getItem('token');
+        const payload = {
+            'questions': getNewQuestions(),
+            'name': props.quizName,
+            'thumbnail': props.thumbnail
+        }
+        const options = {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        }
+        const path = `${config.basePath}/admin/quiz/${quizID}`;
+        fetch(path, options)
+            .then(res => {
+                if (!res.ok) {
+                    throw res;
+                }
+                console.log('Good delete');
+                props.setQuestionChange(!props.questionChange);
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 
     return (
         <Card>
             <Card.Content>
                 <Card.Header content={json.question} />
-                <Card.Meta content='Musicians' />
-                <Card.Description content='Jake is a drummer living in New York.' />
+                <Card.Meta content={json.type} />
+                <Card.Description content='Some content' />
                 <Button onClick={deleteQuestion}>Delete Question</Button>
             </Card.Content>
         </Card>
@@ -29,7 +67,12 @@ function QuestionCard(props) {
 }
 
 QuestionCard.propTypes = {
-  json : PropTypes.any
+  json : PropTypes.any,
+  questionChange: PropTypes.bool,
+  setQuestionChange: PropTypes.func,
+  questions: PropTypes.array,
+  quizName: PropTypes.string,
+  thumbnail: PropTypes.string
 }
 
 export default QuestionCard;
